ae26ff64599a4e39b3045814130b139527842fd7
epovarova <gulkina_liza@mail.ru>
2018-10-30 22:42:51 +0300

    HW3

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -1,9 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/get_classifier_result/<version>", methods=['GET', 'POST'])
 def return_classifier_result(version):
+    if request.method == 'POST':
+        value = request.json
+        if version=='1':
+            data = {"version":1,"predict":value['predict']}
+        if version=='0':
+            data = {"version":0,"predict":value['old_predict']}
+        return jsonify(data)
     #TODO прочитать из полученного запроса json-контент
     #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
     #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
@@ -11,7 +18,7 @@ def return_classifier_result(version):
 @app.route("/")
 def hello():
     #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run()
